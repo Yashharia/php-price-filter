2e2d14d715ffc03a91a2028cfd988f0de3e75c3c
YashHaria <yash.rh@somaiya.edu>
2024-04-04 20:09:58 +0530

    update with table and images, and files list

--- helper/upload.php
+++ helper/upload.php
@@ -4,6 +4,12 @@ require '../vendor/autoload.php';
 
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
+// Make sure the 'uploads' directory exists and is writable
+$uploadDir ='../uploads/sheets';
+if (!is_dir($uploadDir)) {
+    mkdir($uploadDir, 0777, true);
+}
+
 function insertProductData($data, $mysqli, $headers, $filename)
 {
     // Prepare your SQL statement based on the headers, example:
@@ -14,8 +20,8 @@ function insertProductData($data, $mysqli, $headers, $filename)
         $name =  $row['ITEM DESCRIPTION'];
         $pricing =  (float) str_replace("$", "", $row['UNIT PRICE']);
         $supplier_name =  $filename;
-        $upc =  $row['UPC'];
-        if($name == "" || $pricing == "") continue;
+        $upc =  ltrim($row['UPC'],0);
+        if ($name == "" || $pricing == "") continue;
 
         print_r($filename);
         print_r($row);
@@ -26,42 +32,78 @@ function insertProductData($data, $mysqli, $headers, $filename)
     $stmt->close();
 }
 
+function insertFileData($mysqli, $name, $tmpName)
+{
+    $stmt = $mysqli->prepare('INSERT INTO files (name, filepath) VALUES (?,?)');
+    $stmt->bind_param('ss', $name, $tmpName);
+    $stmt->execute();
+    $stmt->close();
+}
+
+function truncate_table($tableName)
+{
+    global $mysqli;
+    $query = "TRUNCATE TABLE `$tableName`";
+    if ($mysqli->query($query) === TRUE) {
+        echo "Table $tableName has been emptied successfully.";
+    } else {
+        echo "Error emptying table: " . $mysqli->error;
+    }
+}
+
+
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_FILES['file']['name'] && !empty($_POST['fileName']))) {
+
     if ($mysqli->connect_error) {
         die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
     }
 
+    truncate_table('files');
+    truncate_table('products');
+
     $filename = $_POST['fileName'];
     $count = 0;
+
     foreach ($_FILES['file']['tmp_name'] as $index => $tmpName) {
-        $spreadsheet = IOFactory::load($tmpName);
-        $worksheet = $spreadsheet->getActiveSheet();
 
-        // Get headers from the first row
-        $headerRow = $worksheet->getRowIterator(1)->current();
-        $cellIterator = $headerRow->getCellIterator();
-        $cellIterator->setIterateOnlyExistingCells(true);
-        $headers = [];
-        foreach ($cellIterator as $cell) {
-            $column_heading = strtoupper($cell->getValue());
-            $headers[$column_heading] = $cell->getColumn();
-        }
+        // Generate a unique file name to prevent overwriting existing files
+        $originalName = $_FILES['file']['name'][$index];
+        $targetFilePath = $uploadDir . '/' . basename($originalName);
 
-        // Read data rows
-        $data = [];
-        foreach ($worksheet->getRowIterator(2) as $row) { // Start from the second row
-            $cellIterator = $row->getCellIterator();
+        print_r($targetFilePath);
+        if (move_uploaded_file($tmpName, $targetFilePath)) {
+
+            $spreadsheet = IOFactory::load($targetFilePath);
+            $worksheet = $spreadsheet->getActiveSheet();
+
+            // Get headers from the first row
+            $headerRow = $worksheet->getRowIterator(1)->current();
+            $cellIterator = $headerRow->getCellIterator();
             $cellIterator->setIterateOnlyExistingCells(true);
-            $rowData = [];
+            $headers = [];
             foreach ($cellIterator as $cell) {
-                $header = array_search($cell->getColumn(), $headers);
-                $rowData[$header] = $cell->getValue();
+                $column_heading = strtoupper($cell->getValue());
+                $headers[$column_heading] = $cell->getColumn();
             }
-            $data[] = $rowData;
-        }
 
-        insertProductData($data, $mysqli, $headers, $filename[$count]);
-        $count++;
+            // Read data rows
+            $data = [];
+            foreach ($worksheet->getRowIterator(2) as $row) { // Start from the second row
+                $cellIterator = $row->getCellIterator();
+                $cellIterator->setIterateOnlyExistingCells(true);
+                $rowData = [];
+                foreach ($cellIterator as $cell) {
+                    $header = array_search($cell->getColumn(), $headers);
+                    $rowData[$header] = $cell->getValue();
+                }
+                $data[] = $rowData;
+            }
+
+            insertFileData($mysqli, $filename[$count], $originalName);
+            insertProductData($data, $mysqli, $headers, $filename[$count]);
+            $count++;
+        }
     }
 
     $mysqli->close();
--- index.php
+++ index.php
@@ -1,11 +1,25 @@
 <?php include 'header.php'; ?>
 <div class="container text-center">
+    <?php $files_sql = "SELECT * from files";
+    $files_result = $mysqli->query($files_sql);  ?>
     <form id="uploadForm" enctype="multipart/form-data">
         <div id="fileUploads">
             <div class="file-upload my-2">
                 <input type="text" name="fileName[]" required placeholder="File Name" class="form-control mx-2">
                 <input type="file" name="file[]" required accept=".xls,.xlsx" class="form-control mx-2">
             </div>
+            <?php if ($files_result->num_rows > 0) {
+                while ($row = $files_result->fetch_assoc()) { ?>
+                    <div class="file-upload row my-2">
+                        <div class="col-6">
+
+                            <input type="text" name="fileName[]" value="<?php echo $row['name'] ?>" required placeholder="File Name" class="form-control mx-2 col-6">
+                        </div>
+                        <div class="col-6"><?php echo  $row['filepath'] ?></div>
+                    </div>
+            <?php }
+            } ?>
+
         </div>
         <button type="button" class="btn btn-primary" id="addMore">Add More Files</button>
         <input type="submit" class="btn btn-primary" value="Upload Files">
@@ -43,7 +57,7 @@ ORDER BY
             // Output data of each row
             while ($row = $result->fetch_assoc()) {
                 echo "<tr>";
-                echo "<td></td>";
+                echo "<td><img src='./uploads/" . $row["upc"] . ".jpg' class='product-img'/></td>";
                 echo "<td>" . $row["name"] . "</td>";
                 echo "<td>" . $row["upc"] . "</td>";
                 // Split the price_supplier string into individual price-supplier pairs
@@ -52,8 +66,8 @@ ORDER BY
                 foreach ($priceSuppliers as $priceSupplier) {
                     // Split each pair into price and supplier
                     [$price, $supplier] = explode(' - ', $priceSupplier);
-                    $formattedPrice = number_format((float)$price, 4, '.', '');
-                    echo "<input type='radio' name='" . $row["upc"] . "' data-supplier='" . $supplier . "'> $" . $formattedPrice ." - ". $supplier. "</input><br>";
+                    $formattedPrice = number_format((float)$price, 2, '.', '');
+                    echo "<input type='radio' name='" . $row["upc"] . "' data-supplier='" . $supplier . "'> $" . $formattedPrice . " - " . $supplier . "</input><br>";
                 }
                 echo "</td>";
 
